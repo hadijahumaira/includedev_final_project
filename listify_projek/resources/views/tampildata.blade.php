e3ea30626f4c34aec3ffd6cd6797238eeb59cd11
Dhaifina Alifa Putri <alifa211@mhs.unsyiah.ac.id>
2023-05-18 07:46:26 +0700

    add : view tampildata for editing data

---
+++ listify_projek/resources/views/tampildata.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.admin')
+
+@section('content')
+
+<body>
+    <h1 class="text-center mb-4">Edit To dO list</h1>
+
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="/updatedata/{{ $data->id }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Title</label>
+                                <input type="text" name="nama" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" value="{{ $data->nama }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Status</label>
+                                <select class="form-select form-control" name="status" aria-label="Default select example">
+                                    <option selected>{{ $data->status }}</option>
+                                    <option value="ToDo">ToDo</option>
+                                    <option value="Doing">Doing</option>
+                                    <option value="Done">Done</option>
+
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Deadline</label>
+                                <input type="date" name="deadline" class="form-control" value="{{ date('Y-m-d', strtotime($data->deadline)) }}">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+</body>
+
+@endsection
